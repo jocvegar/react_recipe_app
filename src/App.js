ab48383fb821916c4cb8196c66a00c1499d46d0e
jocvegar <jocvegar@gmail.com>
2019-05-16 15:09:55 -0600

    Adds styling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import './App.css';
 const App = () => {
 	const APP_ID = 'd65a23aa';
 	const APP_KEY = '641b2187379ae1049339e4e4cfac7b54';
-	const exampleUrl = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 	const [recipes, setRecipes] = useState([]);
 	const [search, setSearch] = useState('');
+	const [query, setQuery] = useState('');
+
+	const exampleUrl = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 	useEffect(() => {
 		getRecipes();
 		console.log("Effect has been run");
-	}, []);
-
+	}, [query]);
 
 	const getRecipes = async () => {
 		const response = await fetch(exampleUrl)
@@ -23,13 +24,21 @@ const App = () => {
 		console.log(data.hits);
 	}
 
-	const handleChange()
+	const handleChange = (e) => {
+		setSearch(e.target.value);
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setQuery(search);
+	}
 
 	return (
 		<div className="App">
-			<h1>Hello React!</h1>
-			<form className="search-form">
-				<input className="search-bar" type="text" value={search} onChange={} />
+			<h1>Find something to cook</h1>
+			<form className="search-form" onSubmit={handleSubmit}>
+				<input className="search-bar" type="text" value={search} onChange={handleChange}
+					placeholder="What are we cooking today?" />
 				<button className="search-button" type="submit">
 					Search
 				</button>
@@ -40,6 +49,7 @@ const App = () => {
 					calories = {recipe.recipe.calories}
 					image = {recipe.recipe.image}
 					key = {recipe.recipe.url}
+					ingredients = {recipe.recipe.ingredients}
 				/>
 			))}
 		</div>
